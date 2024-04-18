e41cae9cb0abd7678b3efc6232b4ec016fa7cd19
walpRKA <145676508+walpRKA@users.noreply.github.com>
2024-04-18 18:27:58 +0200

    animalmovement begin

--- Assets/Lilian/AnimatedAnimals/Flying.cs
+++ Assets/Lilian/AnimatedAnimals/Flying.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Flyinh : MonoBehaviour
+public class Flying : MonoBehaviour
+   
 {
-    // Start is called before the first frame update
+    public GameObject Animal;
+    public GameObject[] FlyCheck;
+         // Start is called before the first frame update
     void Start()
     {
-        
+        Animal = GameObject.FindGameObjectWithTag<FlyAnimal>();
     }
 
     // Update is called once per frame
