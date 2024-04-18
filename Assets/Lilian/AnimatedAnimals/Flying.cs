ffee6a6fbcc5cd87891e249f3843e2858f05e5ee
Skyron <courtier.leo@gmail.com>
2024-04-18 22:18:40 +0200

    Animation localisée (presque) finie
    il manque juste quelques ajustements de valeurs :)

--- Assets/Lilian/AnimatedAnimals/Flying.cs
+++ Assets/Lilian/AnimatedAnimals/Flying.cs
@@ -10,7 +10,7 @@ public class Flying : MonoBehaviour
          // Start is called before the first frame update
     void Start()
     {
-        Animal = FindObjectOfType<Flying>();
+        // Animal = FindObjectOfType<Flying>();
     }
 
     // Update is called once per frame
--- Assets/Léo/Scripts/AnimationTrigger.cs
+++ Assets/Léo/Scripts/AnimationTrigger.cs
@@ -1,18 +1,16 @@
 using Unity.XR.CoreUtils;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.XR;
 
 [RequireComponent(typeof(SphereCollider))]
 public class AnimationTrigger : MonoBehaviour
 {
     [SerializeField] private Material groundMaterial;
     [Range(0, 50)] private float transitionFactor;
-    [Range(-1, 0)] private float fresnelImitation;
+    [Range(-.13f, 0)] private float fresnelImitation;
     private float startTime;
     [SerializeField] private float effectDuration;
     [SerializeField] private float deltaInc; 
-    private float deltaFresnelInc;
+    [SerializeField] private float deltaFresnelInc;
     private Vector2 _position;
     private bool _hasStarted;
     private MeshRenderer meshRenderer;
@@ -22,7 +20,6 @@ public class AnimationTrigger : MonoBehaviour
 
     private void Awake() {
         materialPropertyBlock = new MaterialPropertyBlock();
-        deltaFresnelInc = Time.deltaTime / 2;
     }
     
     private void Update() {
@@ -31,31 +28,32 @@ public class AnimationTrigger : MonoBehaviour
 
     private void OnCollisionEnter(Collision other) {
         if(!CollideByBottom(other)) return;
-        ContactPoint contactPoint = other.contacts[0];
-        Ray ray = new Ray(contactPoint.point - contactPoint.normal, contactPoint.normal);
+        Ray ray = new Ray(transform.position, Vector3.down);
         RaycastHit hit = new RaycastHit();
         if (!Physics.Raycast(ray, out hit)) return;
-        Debug.Log(hit.textureCoord);
         _position = hit.textureCoord;
         meshRenderer = other.gameObject.GetComponent<MeshRenderer>();
-        
         meshRenderer.AddMaterial(new Material(groundMaterial));
         _materialIndex = meshRenderer.materials.Length - 1;
-        fresnelImitation = -1;
+        fresnelImitation = -.12f;
+        transitionFactor = 0;
         _hasStarted = true;
     }
     
     private bool CollideByBottom(Collision other) => other.transform.position.y < transform.position.y;
+
+    private float UVToPos(float pUVCoordinates) => -5 + (1 - pUVCoordinates) * 10;
     
     private void LaunchTransition() {
-       startTime = Time.time;
+        if(meshRenderer == null) return;
+        startTime = Time.time;
         transitionFactor += deltaInc;
         transitionFactor = Mathf.Clamp(transitionFactor, 0, 50);
         fresnelImitation += deltaFresnelInc;
-        fresnelImitation = Mathf.Clamp(fresnelImitation, -1, 0);
+        fresnelImitation = Mathf.Clamp(fresnelImitation, -.12f, 0);
         meshRenderer.GetPropertyBlock(materialPropertyBlock, _materialIndex);
         materialPropertyBlock.SetFloat("_transitionFactor", transitionFactor);
-        materialPropertyBlock.SetVector("_Position", new Vector3(_position.x,fresnelImitation , _position.y));
+        materialPropertyBlock.SetVector("_Position", new Vector3(UVToPos(_position.x),fresnelImitation, UVToPos(_position.y)));
         meshRenderer.SetPropertyBlock(materialPropertyBlock, _materialIndex);
         if (startTime - Time.time > effectDuration) enabled = true;
     }
