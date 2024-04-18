d742b0f9ae2f0188493b97d53355c532a726fa52
Domingues romain <romain.Jvfan@gmail.com>
2024-04-18 10:43:24 +0200

    Les petits cubes spawn et tombes

---
+++ Assets/Romain/Spawner.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections; // N'oublie pas d'importer cet espace de noms
+
+public class CubeSpawner : MonoBehaviour
+{
+    public GameObject cubePrefab; // Prefab du cube à spawn
+    public Vector3 mapSize = new Vector3(120f, 10f, 120f); // Taille de la carte
+
+    void Start()
+    {
+        // Lancer la coroutine pour faire spawn les cubes
+        StartCoroutine(SpawnCubesRoutine());
+    }
+
+    IEnumerator SpawnCubesRoutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(5f); // Attendre 5 secondes
+
+            SpawnCubeRandomPosition();
+        }
+    }
+
+    void SpawnCubeRandomPosition()
+    {
+        Debug.Log("oui");
+        // Générer une position aléatoire sur la carte
+        Vector3 randomPosition = GenerateRandomPosition();
+
+        // Spawn un cube à la position aléatoire
+        Instantiate(cubePrefab, randomPosition, Quaternion.identity);
+    }
+
+    Vector3 GenerateRandomPosition()
+    {
+        // Générer des coordonnées aléatoires dans les limites de la carte
+        float randomX = Random.Range(-mapSize.x / 2f, mapSize.x / 2f);
+        float randomZ = Random.Range(-mapSize.z / 2f, mapSize.z / 2f);
+
+        // La hauteur Y peut être ajustée selon tes besoins
+        float randomY = Random.Range(7f, 20);
+
+        // Retourner les coordonnées aléatoires dans un Vector3
+        return new Vector3(randomX, randomY, randomZ);
+    }
+}
