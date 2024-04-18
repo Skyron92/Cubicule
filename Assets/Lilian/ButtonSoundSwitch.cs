01c9823b2a58015581a2f8506c4504bd23c4c93a
walpRKA <145676508+walpRKA@users.noreply.github.com>
2024-04-18 14:57:16 +0200

    fade out fonctionnel
    a en chialer

--- Assets/Lilian/ButtonSoundSwitch.cs
+++ Assets/Lilian/ButtonSoundSwitch.cs
@@ -5,20 +5,22 @@ using UnityEngine;
 
 
 public class ButtonSoundSwitch : MonoBehaviour
-    { 
+{
     private GameObject citySoundSource;
     public AudioSource[] Sounds;
     private float[] initialVolumes;
     public float fadeDuration = 2f;
     private float fadeTimer;
+    private bool ActiveButton = false;
 
 
     private void Start()
     {
         citySoundSource = GameObject.FindGameObjectWithTag("CitySound");
         Sounds = citySoundSource.GetComponents<AudioSource>();
+        Debug.Log("je trouve : " + Sounds);
 
-        { 
+
         initialVolumes = new float[Sounds.Length];
         for (int i = 0; i < Sounds.Length; i++)
         {
@@ -26,33 +28,48 @@ public class ButtonSoundSwitch : MonoBehaviour
         }
 
         fadeTimer = 0f;
+
+
+
     }
 
 
-}
-    public void CityFade()
+    public void ButtonPressed()
     {
-        if (fadeTimer < fadeDuration)
-        {
-            
-            float progress = fadeTimer / fadeDuration;
-            for (int i = 0; i < Sounds.Length; i++)
-            {
-                float newVolume = Mathf.Lerp(initialVolumes[i], 0f, progress);
-                Sounds[i].volume = newVolume;
-            }
+        ActiveButton = true;
+    }
 
-            
-            fadeTimer += Time.deltaTime;
-        }
-        else
+    private void Update()
+    {
+
+        if (ActiveButton == true)
         {
-            
-            for (int i = 0; i < Sounds.Length; i++)
+            if (fadeTimer < fadeDuration)
             {
-                Sounds[i].Stop();
+                Debug.Log("test");
+
+
+                float progress = fadeTimer / fadeDuration;
+                for (int i = 0; i < Sounds.Length; i++)
+                {
+                    float newVolume = Mathf.Lerp(initialVolumes[i], 0f, progress);
+                    Sounds[i].volume = newVolume;
+                }
+
+
+                fadeTimer += Time.deltaTime;
+            }
+            else
+            {
+
+                for (int i = 0; i < Sounds.Length; i++)
+                {
+                    Sounds[i].Stop();
+                    ActiveButton = false;
+                }
             }
         }
     }
 }
+    
 
