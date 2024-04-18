5cca860f446dc1a855996836bc9b9989eabeeaf5
Skyron <courtier.leo@gmail.com>
2024-04-18 18:29:37 +0200

    Continue animation

--- Assets/Léo/Scripts/AnimationTrigger.cs
+++ Assets/Léo/Scripts/AnimationTrigger.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using Unity.XR.CoreUtils;
 using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR;
 
 [RequireComponent(typeof(SphereCollider))]
 public class AnimationTrigger : MonoBehaviour
@@ -31,13 +33,14 @@ public class AnimationTrigger : MonoBehaviour
 
     private void OnCollisionEnter(Collision other) {
         if(!CollideByBottom(other)) return;
-        Debug.Log("collided");
         ContactPoint contactPoint = other.contacts[0];
         Ray ray = new Ray(contactPoint.point - contactPoint.normal, contactPoint.normal);
         RaycastHit hit = new RaycastHit();
         if (!Physics.Raycast(ray, out hit)) return;
+        Debug.Log(hit.textureCoord);
         _position = hit.textureCoord;
         meshRenderer = other.gameObject.GetComponent<MeshRenderer>();
+        
         meshRenderer.AddMaterial(new Material(groundMaterial));
         _materialIndex = meshRenderer.materials.Length - 1;
         fresnelImitation = -1;
@@ -54,7 +57,7 @@ public class AnimationTrigger : MonoBehaviour
         fresnelImitation = Mathf.Clamp(fresnelImitation, -1, 0);
         meshRenderer.GetPropertyBlock(materialPropertyBlock, _materialIndex);
         materialPropertyBlock.SetFloat("_transitionFactor", transitionFactor);
-        materialPropertyBlock.SetVector("_Position", new Vector4(_position.x,fresnelImitation , _position.y, 0));
+        materialPropertyBlock.SetVector("_Position", new Vector3(_position.x,fresnelImitation , _position.y));
         meshRenderer.SetPropertyBlock(materialPropertyBlock, _materialIndex);
         if (startTime - Time.time > effectDuration) enabled = true;
     }
