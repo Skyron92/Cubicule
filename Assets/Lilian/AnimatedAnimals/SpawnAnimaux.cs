2ed065058e4db39548a56e018a89c77ed81658f1
Skyron <courtier.leo@gmail.com>
2024-05-03 15:15:01 +0200

    vwala

--- Assets/Lilian/AnimatedAnimals/SpawnAnimaux.cs
+++ Assets/Lilian/AnimatedAnimals/SpawnAnimaux.cs
@@ -1,13 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using Random = UnityEngine.Random;
 
 public class SpawnAnimaux : MonoBehaviour
 {
     public GameObject[] animalPrefabs; // Tableau des prefabs d'animaux disponibles
     public Vector3 mapSize = new Vector3(120f, 0f, 120f); // Taille de la carte
     public int numberOfCubes = 10; // Nombre de cubes � instancier
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("Jump")) Spawn();
+    }
 
     // Changez la visibilite de la methode Start() a public
     public void Spawn()
@@ -31,7 +38,7 @@ public class SpawnAnimaux : MonoBehaviour
         float randomZ = Random.Range(-mapSize.z / 2f, mapSize.z / 2f);
 
         // La hauteur Y peut �tre ajust�e selon tes besoins
-        float randomY = 0;
+        float randomY = 0.03f;
 
         // Retourner les coordonnees aleatoires dans un Vector3
         return new Vector3(randomX, randomY, randomZ);
